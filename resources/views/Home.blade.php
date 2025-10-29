c0e18d6ecd855088dfb26120cce3fe14bde29ce7
SQFr3aky <fr3aky009@gmail.com>
2025-10-29 22:01:23 +0700

    bug update profile

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -5,11 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}"> 
     <title>{{ config('app.name', 'BeliIn') }} E-commerce Final</title>
-    
-    {{-- Memuat CSS dan JS --}}
+     
     @vite(['resources/css/Home.css', 'resources/js/app.js']) 
 
-    {{-- Memuat Ion-icons --}}
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     
@@ -34,8 +32,7 @@
                     </button>
                 </div>
             </div>
-            
-    
+
             <div class="navbar-right">
                 @if (Route::has('login'))
                     @auth
